aee29615534f1f8256ec5521a38b141b2d01a004
NekoMeow <nopeople123456789@gmail.com>
2020-10-28 20:43:43 +0800

    Move Script and Update Size

---
+++ script/dummy.js
@@ -0,0 +1,12 @@
+function dummyFunc()
+{
+    var x = document.getElementById("topNav");
+    if (x.className == "topnav")
+    {
+        x.className += " responsive";
+    }
+    else
+    {
+        x.className = "topnav";
+    }
+}
