31fd01635af642d401b03061d3afa24986960d00
NekoMeow <nopeople012345@gmail.com>
2020-10-31 10:53:47 +0800

    Move to script, update css and main html

---
+++ script/countdown.js
@@ -0,0 +1,35 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 10, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var countdownfunction = setInterval(function()
+{
+    // Get todays date and time
+    var now = new Date().getTime();
+  
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Output the result in an element with id="demo"
+    document.getElementById("estimateDate1").innerHTML = d + "d " + h + "h "
+    + m + "m " + s + "s";
+    document.getElementById("estimateDate2").innerHTML = d + "d " + h + "h "
+    + m + "m " + s + "s";
+    document.getElementById("estimateDate3").innerHTML = d + "d " + h + "h "
+    + m + "m " + s + "s";
+  
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+
+        document.getElementById("estimateDate1").innerHTML = "EXPIRED - Contact Owner if still no updating?";
+        document.getElementById("estimateDate2").innerHTML = "EXPIRED - Contact Owner if still no updating?";
+        document.getElementById("estimateDate3").innerHTML = "EXPIRED - Contact Owner if still no updating?";
+  }
+}, 1000);
---
+++ script/updateNavBtn.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var btns = document.getElementById("topNav").getElementsByClassName("navBtn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
